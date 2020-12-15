361154c158126ada9fe89458d2c51700500b65c8
stanisuravn <korolenko-stanislav@mail.ru>
2020-12-16 01:32:39 +0500

    Изменение на главной странице списка лабораторных работ

--- index.php
+++ index.php
@@ -13,11 +13,11 @@
     <h1 id="h1">Список лабораторных работ Короленко С.С.</h1>
 
     <ul id="nav">
-      <li><a href="WWW/lab1/">Лаба 1</a>
-      <li><a href="WWW/lab2/">Лаба 2</a>
-      <li><a href="WWW/lab3/">Лаба 3</a>
-      <li><a href="WWW/lab4/">Лаба 4</a>
-      <li><a href="WWW/lab5/">Лаба 5</a>
+      <li><a href="WWW/lab1/">Лабораторная 1</a>
+      <li><a href="WWW/lab2/">Лабораторная 2</a>
+      <li><a href="WWW/lab3/">Лабораторная 3</a>
+      <li><a href="WWW/lab4/">Лабораторная 4</a>
+      <li><a href="WWW/lab5/">Лабораторная 5</a>
     </ul>
   </div>
   </body>
